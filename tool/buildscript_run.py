392c2a058828d3d47a3ad050e6a319235bc407f2
Luxian <lux1an@qq.com>
2026-07-29 21:55:42 +0800

    fix-native-buildscript-linking

--- tool/buildscript_run.py
+++ tool/buildscript_run.py
@@ -322,9 +322,11 @@ def main() -> None:  # noqa: C901
             path = cargo_rustc_link_search_match.group(2)
             if path.startswith(TOOL_CWD):
                 relative_path = path[len(TOOL_CWD) :]
-                flags += f"-L{kind}$(abspath {relative_path})\n"
+                flags += f"-L{kind}{relative_path}\n"
+            elif not os.path.isabs(path):
+                flags += f"-L{kind}{path}\n"
             else:
-                # Disregard link search not located within the build script's out dir.
+                # Disregard absolute link search paths outside the workspace.
                 pass
             continue
         # *BUCKAL-ONLY* metadata processing
